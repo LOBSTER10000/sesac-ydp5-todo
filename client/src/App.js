2c9c074c07ff195531478d9825d496fcf1924f79
LOBSTER10000 <ghh112034@gmail.com>
2023-10-11 18:43:48 +0900

    todolist todo 추가하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Todo from './components/Todo';
+import AddTodo from './components/AddTodo';
 import { useState } from 'react';
 
 function App() {
@@ -19,13 +20,30 @@ function App() {
       title: 'my todo3',
       done: true,
     },
+    {
+      id: 4,
+      title: '백엔드 프로젝트 완성하기',
+      done: false,
+    },
   ]);
+
+  //todoItems 상태에 새로운 투두를 추가하는 일
+  const addItem = (newItem) => {
+    console.log(newItem); // {title : 저녁 먹기}
+    // newItem id 키 값 넣고, newItem done 키값
+    newItem.id = todoItems.length + 1;
+    newItem.done = false;
+    // todoItems 배열에 newItem을 추가
+    setTodoItems([...todoItems, newItem]);
+  };
 
   return (
     <div className="App">
+      <AddTodo addItem={addItem} />
       {/* todoItem 반복, props 데이터 (투두 객체)를 자식 컴포넌트에게 전달 */}
-      <Todo />
-      <Todo />
+      {todoItems.map((item) => (
+        <Todo key={item.id} item={item} />
+      ))}
     </div>
   );
 }
