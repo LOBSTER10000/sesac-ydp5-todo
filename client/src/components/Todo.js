967cd818a86fb64e96ddc6d2077c9c8c3ace9c0e
LOBSTER10000 <ghh112034@gmail.com>
2023-10-13 14:05:04 +0900

    study : todolist 수정

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function todo({ item, deleteItem }) {
-  const { id, title, done } = item;
+export default function Todo({ item, deleteItem }) {
+  const [todoItem, setTodoItem] = useState(item);
+  const { id, title, done } = todoItem;
+  const [readOnly, setReadOnly] = useState(true);
 
   const onDeleteButtonClick = () => {
-    deleteItem(id);
+    deleteItem(todoItem);
+  };
+
+  // title 클릭하면 readonly를 false로 변경(수정 가능하도록!!)
+  const offReadOnlyMode = () => {
+    setReadOnly(false);
+  };
+
+  // title 수정
+  const editEventHandler = (e) => {
+    const { title, ...rest } = todoItem;
+    setTodoItem({
+      title: e.target.value,
+      ...rest,
+    });
+  };
+
+  const editKeyEventHandler = (e) => {
+    if (e.keyCode === 13) {
+      setReadOnly(true);
+    }
+  };
+
+  // 체크박스 상태 업데이트
+  const checkboxEventHandler = (e) => {
+    const { done, ...rest } = todoItem;
+    setTodoItem({
+      done: e.target.checked,
+      ...rest,
+    });
   };
 
   return (
@@ -14,8 +45,17 @@ export default function todo({ item, deleteItem }) {
         name={`todo${id}`}
         id={`todo${id}`}
         defaultChecked={done}
+        onChange={checkboxEventHandler}
       />
-      <label htmlFor={`todo${id}`}>{title}</label>
+      {/* <label htmlFor={`todo${id}`}>{title}</label> */}
+      <input
+        type="text"
+        value={todoItem.title}
+        readOnly={readOnly}
+        onClick={offReadOnlyMode}
+        onChange={editEventHandler}
+        onKeyDown={editKeyEventHandler}
+      />
       <button onClick={onDeleteButtonClick}>DELETE</button>
     </div>
   );
